fd37393fe5e6eb1647e8d96a722caf4af09f90fc
303275 <148846545+303275@users.noreply.github.com>
2023-10-29 21:52:23 +0100

    Add files via upload
    Programowanie Lab1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,94 @@
-class Wektor2D
-{
-    // Tutaj napisz implementacje klasy Wektor2D
+﻿#include <iostream>
+#include <cmath>
+
+#define M_PI 3.14159265358979323846
+
+class Informer {
+public:
+    Informer() {
+        std::cout << "Informer: Konstrukcja" << std::endl;
+    }
+
+    ~Informer() {
+        std::cout << "Informer: Destrukcja" << std::endl;
+    }
 };
+
+class Wektor2D {
+private:
+    double x;
+    double y;
+    Informer informer;
+    static int num_wek;
+
+    
+
+public:
+    Wektor2D(double nx, double ny) {
+        x = nx;
+        y = ny;
+        num_wek++;
+    }
+    Wektor2D() {
+        x = 0.0;
+        y = 0.0;
+    }
+    static Wektor2D kart(double x, double y) {
+        return Wektor2D(x, y);
+    }
+
+    static Wektor2D bieg(double r, double theta) {
+        double x = r * std::cos(theta);
+        double y = r * std::sin(theta);
+        return Wektor2D(x, y);
+    }
+
+    double norm() {
+        return std::sqrt(x * x + y * y);
+    }
+
+    void print() {
+        std::cout << "Wektor: (" << x << ", " << y << ")" << std::endl;
+    }
+
+    ~Wektor2D() {
+        num_wek--;
+    }
+
+    void setX(double newX) {
+        x = newX;
+    }
+
+    double getX() {
+        return x;
+    }
+
+    void setY(double newY) {
+        y = newY;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    Wektor2D operator+(const Wektor2D& other) const {
+        return Wektor2D(x + other.x, y + other.y);
+    }
+    double operator*(const Wektor2D& other) const {
+        return (x * other.x + y * other.y);
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Wektor2D& wektor) {
+        os << "Wektor: (" << wektor.x << ", " << wektor.y << ")";
+        return os;
+    }
+
+    static int populacja() {
+        return num_wek;
+    }
+};
+
+int Wektor2D::num_wek = 0;
+
+int main() {
+    
+}
